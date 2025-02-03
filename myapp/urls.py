24732dc5dbbb2f77b572dd384f1c7176b5910ecd
sanvirahman <sheikh15-3700@diu.edu.bd>
2025-02-03 18:11:51 +0600

    init now

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register
+from .views import landing,contact
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('landing/', landing, name='landing'),
+    path('contact/',contact,name="contact")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from .forms import CustomUserCreationForm
 
 
 # Create your views here.
-def register(request):
+def landing(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -12,3 +12,13 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, "index.html", {"form": form})
+
+def contact(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("contact")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "contact.html", {"form": form})
